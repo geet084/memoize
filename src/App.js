bbc01e9f40314ad9ca9b38dbcf4de6bf0e29d3eb
Travis Gee <geet084@gmail.com>
2019-01-04 10:34:15 -0700

    Reorganize file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './CSS/Main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>HELLO WORLD</div>
+      <main className="App">
+        hello
+      </main>
     );
   }
 }
