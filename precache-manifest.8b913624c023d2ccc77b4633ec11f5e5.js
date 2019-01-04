c1a868dab5e7f663a27b39455f89bffd63937189
Travis Gee <geet084@gmail.com>
2019-01-04 08:41:43 -0700

    Updates

---
+++ precache-manifest.8b913624c023d2ccc77b4633ec11f5e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memoize/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a418a8cbff2e33a9fcc",
+    "url": "/memoize/static/js/runtime~main.1a418a8c.js"
+  },
+  {
+    "revision": "b48ae15c0a2d7cb6a677",
+    "url": "/memoize/static/js/main.b48ae15c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/memoize/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b48ae15c0a2d7cb6a677",
+    "url": "/memoize/static/css/main.2a45ea20.chunk.css"
+  },
+  {
+    "revision": "091d8ef25518d326bbf42c66a8ac3d28",
+    "url": "/memoize/index.html"
+  }
+];
