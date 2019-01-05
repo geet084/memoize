9e498734075515d1cc6dff7c62236eef38272186
Travis Gee <geet084@gmail.com>
2019-01-05 16:27:15 -0700

    Add nav and display functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import './CSS/Main.scss';
+import Display from './Display.js';
+import Nav from './Nav.js';
+import prototypes from './datasets/aType.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      prototypes: prototypes,
+      selection: 'all'
+    }
+  }
+
+  updateType = (e) => {
+    if (e.target.value !== undefined) {
+      this.setState({ selection: e.target.value })
+    }
+    if (e.target.value === 0) {
+      this.setState({ selection: e.target.className })
+    }
+  }
+
   render() {
+    let { prototypes, selection } = this.state;
     return (
       <main className="App">
-        hello
+        <h1>Welcome ....</h1>
+        <Nav prototypes={prototypes}
+             showCard={this.showCard}
+             updateSelection={this.updateType} />
+        {/* <Display prototypes={prototypes[selection]} /> */}
+        <Display prototypes={prototypes}
+                 selection={selection} />
       </main>
     );
   }
