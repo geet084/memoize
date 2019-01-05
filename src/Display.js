a2452abb6f7a31761eccd8a46648fd548f786aae
Travis Gee <geet084@gmail.com>
2019-01-05 16:27:33 -0700

    Add display functionality

---
+++ src/Display.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './CSS/Main.scss';
+import Card from './Card.js'
+
+export default class Display extends Component {
+
+  findCategory = (choice) => { 
+    let types = ['all', 'aTypes', 'sTypes']
+
+    return types.find(type => {
+      return type === choice;
+    })
+  }
+
+  validateInput = (index) => {
+    if (index === -1) return false
+    else return true
+  }
+
+  getIndex = (prototypes, selection) => {
+    return prototypes.all.findIndex(i => {
+      return Object.keys(i)[0].includes(selection)
+    })
+  }
+
+  
+  render() {
+    let { prototypes, selection } = this.props;
+    let categoryChosen = this.findCategory(selection);
+
+    if (categoryChosen) {
+      return (
+        <section>
+          <p>PROTOTYPES SELECTED</p>
+          <Card deets={this.props} />
+        </section>
+      )
+    } else {
+      let index = this.getIndex(prototypes, selection)
+      return (
+        <section>
+          <Card deets={this.props}
+                searchFound={this.validateInput(index)}
+                index={index} />
+        </section>
+      )
+    }
+  }
+}
