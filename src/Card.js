40f3dfd720c6282454a8a32fbffd9958562bdc93
Travis Gee <geet084@gmail.com>
2019-01-05 18:52:50 -0700

    Add the ability to generate random answers and click

--- src/Card.js
+++ src/Card.js
@@ -3,17 +3,47 @@ import './CSS/Main.scss';
 
 export default class Card extends Component {
 
+  getRandom = (max) => {
+    return Math.floor(Math.random() * max)
+  }
+
+  showQuestion = (prototypes, selection) => {
+    let max = prototypes[selection].length
+    let random = this.getRandom(max)
+
+    let arr = [];
+    let right = Object.keys(prototypes[selection][random]).shift()
+    let definition = Object.values(prototypes[selection][random]).shift()
+    let wrong = this.generateWrongAnswers(max, random, prototypes, selection)
+    let answers = [right, ...wrong];
+    
+    arr.push(definition, answers)
+    return arr;
+  }
+
+  generateWrongAnswers = (max, random, prototypes, selection) => {
+    let i = 0;
+    let arr = [];
+    while (i < 3) {
+      let j = this.getRandom(max)
+      if (j !== random) {
+        arr.push(Object.keys(prototypes[selection][j]).shift())
+        i++;
+      }
+    };
+    return arr;
+  }
   
   render() {
-    let { index, searchFound } = this.props;  
+    let { index, searchFound, checkAnswer } = this.props;  
     let { prototypes, selection } = this.props.deets;
     let { all } = this.props.deets.prototypes;
 
     if (searchFound) {
       return (
         <div>
-          <p>{Object.keys(all[index])[0]+ '()'}</p>
-          <p>{Object.values(all[index])[0]}</p>
+          <p>{Object.keys(all[index]).shift()+ '()'}</p>
+          <p>{Object.values(all[index]).shift()}</p>
         </div>
       )
     } else if (!searchFound && !all){
@@ -23,16 +53,18 @@ export default class Card extends Component {
         </div>
       )
     } else {
+      let question = this.showQuestion(prototypes, selection);
       return (
         <div>
           <p>--{selection}</p>
-          {
-            prototypes[selection].map(each => {
-              return <p>
-                       {Object.keys(each)[0]} <br />
-                       {Object.values(each)[0]} </p>
-            })
-          }
+          <p>{question.shift()}</p>
+          <ul>
+            {
+             question.shift().map(answer => {
+               return <li onClick={checkAnswer}>{answer}</li>
+             })
+            }
+          </ul>
         </div>
       )
     }
