3b51e5391b9e1ff928b317f96b95fecf6f196f7e
Travis Gee <geet084@gmail.com>
2019-01-07 14:48:45 -0700

    Change to input for answer

--- src/Nav.js
+++ src/Nav.js
@@ -4,26 +4,29 @@ import './CSS/Main.scss';
 export default class Nav extends Component {
 
   render() {
-    let { updateSelection } = this.props;
+    // let { updateSelection } = this.props;
     return (
       <nav className="nav">
         <ul className="types">
-          <li onClick={updateSelection}
+          <li
+            // onClick={updateSelection}
               className="all">
             All Prototypes
           </li>
-          <li onClick={updateSelection}
+          <li
+            // onClick={updateSelection}
               className="aTypes">
             Array Prototypes
           </li>
-          <li onClick={updateSelection}
+          <li
+            // onClick={updateSelection}
               className="sTypes">
             String Prototypes
           </li>
         </ul>
-        <input type="text"
-               onChange={updateSelection}
-               placeholder="search for specific prototype" />
+        {/* <input type="text"
+              //  onChange={updateSelection}
+               placeholder="search for specific prototype" /> */}
         <ul className="user">
           <li>Your Score</li>
           <li>Reset</li>
