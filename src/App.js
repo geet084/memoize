1dabab2a52b74a2582d33d4937b510fbe20dbcad
Travis Gee <geet084@gmail.com>
2019-01-07 16:29:31 -0700

    Remove advance search options

--- src/App.js
+++ src/App.js
@@ -10,37 +10,24 @@ class App extends Component {
 
     this.state = {
       prototypes: prototypes,
-      selection: 'all',
       answered: []
     }
   }
 
-  updateType = (e) => {
-    if (e.target.value !== undefined) {
-      this.setState({ selection: e.target.value })
-    }
-    if (e.target.value === 0) {
-      this.setState({ selection: e.target.className })
-    }
-  }
-
   answer = (guess, question) => {
-    console.log(this.state.answered)
     let modArr = this.state.answered
     modArr.push({ guess: guess, question: question[0] })
     this.setState({ answered: modArr })
   }
 
   render() {
-    let { prototypes, selection } = this.state;
+    let { prototypes } = this.state;
     return (
       <main className="App">
         <h1>Welcome ....</h1>
         <Nav prototypes={prototypes}
-             showCard={this.showCard}
-             updateSelection={this.updateType} />
+             showCard={this.showCard} />
         <Display prototypes={prototypes}
-                 selection={selection}
                  answer={this.answer}/>
       </main>
     );
