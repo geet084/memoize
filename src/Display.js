74efb61fdd712a1139a364ba36294aaf34bbba4d
Travis Gee <geet084@gmail.com>
2019-01-07 16:30:07 -0700

    Remove advance feature

--- src/Display.js
+++ src/Display.js
@@ -11,48 +11,12 @@ export default class Display extends Component {
     }
   }
 
-  findCategory = (choice) => { 
-    let types = ['all', 'aTypes', 'sTypes']
-
-    return types.find(type => {
-      return type === choice;
-    })
-  }
-
-  validateInput = (index) => {
-    if (index === -1) return false
-    else return true
-  }
-
-  getIndex = (prototypes, selection) => {
-    return prototypes.all.findIndex(i => {
-      return Object.keys(i)[0].includes(selection)
-    })
-  }
-  
   render() {
-
-    let { prototypes, selection } = this.props;
-    let categoryChosen = this.findCategory(selection);
-
-    if (categoryChosen) {
       return (
         <section>
-          <p>--{selection}-- PROTOTYPES SELECTED</p>
-          <Card deets={this.props}
+          <Card questions={this.props.prototypes.aTypes}
             answer={this.props.answer} />
         </section>
       )
-    }
-    // else {
-    //   let index = this.getIndex(prototypes, selection)
-    //   return (
-    //     <section>
-    //       <Card deets={this.props}
-    //             searchFound={this.validateInput(index)}
-    //             index={index} />
-    //     </section>
-    //   )
-    // }
   }
 }
