fc371d4dcda3d985e5bcef7a38b894bbb2d76185
Travis Gee <geet084@gmail.com>
2019-01-07 16:29:01 -0700

    Remove advanced options

--- src/Nav.js
+++ src/Nav.js
@@ -4,29 +4,11 @@ import './CSS/Main.scss';
 export default class Nav extends Component {
 
   render() {
-    // let { updateSelection } = this.props;
     return (
       <nav className="nav">
         <ul className="types">
-          <li
-            // onClick={updateSelection}
-              className="all">
-            All Prototypes
-          </li>
-          <li
-            // onClick={updateSelection}
-              className="aTypes">
-            Array Prototypes
-          </li>
-          <li
-            // onClick={updateSelection}
-              className="sTypes">
-            String Prototypes
-          </li>
+          <li className="aTypes">Practice Array Prototypes</li>
         </ul>
-        {/* <input type="text"
-              //  onChange={updateSelection}
-               placeholder="search for specific prototype" /> */}
         <ul className="user">
           <li>Your Score</li>
           <li>Reset</li>
