2a033b3ec40a3766ec7fc79592fcf71a1e072957
Travis Gee <geet084@gmail.com>
2019-01-08 21:01:42 -0700

    Add tests for display component

--- src/Tests/Display.test.js
+++ src/Tests/Display.test.js
@@ -4,20 +4,29 @@ import { shallow } from 'enzyme';
 import prototypes from '../datasets/aType.js'
 
 const mockData = prototypes;
-const mockAnswer = jest.fn()
+const mockAnswer = jest.fn();
+const mockAnsweredQuestions = jest.fn();
+
 
 describe('Display', () => {
   let wrapper;
 
-  // beforeEach(() => {
-  //   wrapper = shallow(
-  //     <Display prototypes={mockData}
-  //              answer={mockAnswer} />
-  //   )
-  // })
+  beforeEach(() => {
+    wrapper = shallow(
+      <Display prototypes={mockData}
+               currentAnswer={mockAnswer}
+               answeredQuestions={mockAnsweredQuestions} />
+    )
+  })
 
   it('should match the snapshot with all data passed in correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have a proper default state', () => {
+    expect(wrapper.state()).toEqual({
+      correctlyAnswered: null
+    })
   })
 
 });
