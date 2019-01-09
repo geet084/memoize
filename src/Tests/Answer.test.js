3c929ebb07bc1d363598806f1f2c28eef0946de3
Travis Gee <geet084@gmail.com>
2019-01-08 21:02:01 -0700

    Add tests for display component

--- src/Tests/Answer.test.js
+++ src/Tests/Answer.test.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Answer from '../Answer.js';
 import { shallow } from 'enzyme';
 
-const mockClick = jest.fn();
 const mockQuestion = jest.fn();
-const mockAnswer = jest.fn();
-
+const mockAnswer = true;
+const mockCount = 1;
 
 describe('Answer', () => {
   let wrapper;
@@ -13,7 +12,8 @@ describe('Answer', () => {
   beforeEach(() => {
     wrapper = shallow(
       <Answer nextQuestion={mockQuestion}
-              answer={mockAnswer}/>
+              answer={mockAnswer}
+              count={mockCount} />
     )
   })
 
@@ -26,5 +26,23 @@ describe('Answer', () => {
     expect(mockQuestion).toBeCalled()
   })
 
+  it('should not render if the user has not tried to answer a question', () => { 
+    wrapper = shallow(
+      <Answer nextQuestion={mockQuestion}
+        answer={undefined}
+        count={0} />
+    )
+
+    expect(wrapper.html()).toBeNull();
+  })
+
+  it('should show whether or not you guessed right', () => {
+    
+    let result = wrapper.instance().showGuessResult(true, mockCount)
+    expect(result).toEqual("THAT'S CORRECT!")
+
+    result = wrapper.instance().showGuessResult(false, mockCount)
+    expect(result).toEqual("THAT'S INCORRECT!")
+  })
 });
 
